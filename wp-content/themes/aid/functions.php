7c48c03bec3fd597819ef3ea1476bb8d91cbeb13
Nikita <nikitakonurin@gmail.com>
2020-02-17 15:14:19 +0300

    added new scripts and styles:
    - bootstrap 4
    - jquery
    - popper.js

--- wp-content/themes/aid/functions.php
+++ wp-content/themes/aid/functions.php
@@ -126,6 +126,10 @@ function aid_scripts() {
 
 	wp_enqueue_script( 'aid-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_script('aid-popperJs' , 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+
+	wp_enqueue_script('jquery');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
