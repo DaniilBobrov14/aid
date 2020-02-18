1ba86bea80059b1d85eb74694e6c5707afbf4ef8
Nikita <nikitakonurin@gmail.com>
2020-02-18 10:57:40 +0300

    added container and deleted old viewer logo and name of the site

--- wp-content/themes/aid/header.php
+++ wp-content/themes/aid/header.php
@@ -24,18 +24,14 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'aid' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
+	<header id="masthead" class="site-header container">
+		<div class="site-branding row">
+            <div class="col-lg-12">
+                <?php the_custom_logo(); ?>
+                <h1 class="site-title">
+                    Автоматизированный информационный справочник
+                </h1>
+            </div>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
