8ecb14431605493c0adced6dd1995446804aeba7
Nikita <nikitakonurin@gmail.com>
2020-02-18 12:07:43 +0300

    added path to style.css (scss)

--- wp-content/themes/aid/functions.php
+++ wp-content/themes/aid/functions.php
@@ -124,6 +124,8 @@ function aid_scripts() {
 
 	wp_enqueue_style('aid-bootstrap-js', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
 
+	wp_enqueue_style('aid-scss-style' , get_template_directory_uri() . '/css/style.css');
+
 	wp_enqueue_script( 'aid-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'aid-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
