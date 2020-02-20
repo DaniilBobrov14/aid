8b7a5494d8ea5158e1937420d2a0934574b05ed1
Nikita <nikitakonurin@gmail.com>
2020-02-20 10:54:47 +0300

    slider fixed
    -resolution
    -new classes

--- wp-content/themes/aid/front-page.php
+++ wp-content/themes/aid/front-page.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<section content="content-slider">
+<section class="content-slider">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="slider-slick-wrapper">
-                    <img src="/wp-content/themes/aid/assets/img/slider-images/CF000047.jpg" alt="">
-                    <img src="/wp-content/themes/aid/assets/img/slider-images/EP8A5360.JPG" alt="">
-                    <img src="/wp-content/themes/aid/assets/img/slider-images/EP8A5780.jpg" alt="">
+                    <img class="slider-slick-img" src="/wp-content/themes/aid/assets/img/slider-images/CF000047.jpg" alt="">
+                    <img class="slider-slick-img" src="/wp-content/themes/aid/assets/img/slider-images/EP8A5360.JPG" alt="">
+                    <img class="slider-slick-img" src="/wp-content/themes/aid/assets/img/slider-images/EP8A5780.jpg" alt="">
                 </div>
             </div>
         </div>
