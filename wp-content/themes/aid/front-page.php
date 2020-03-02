a4302abcc5a0797bcf26a088c2a5c0f1701ae218
Nikita <nikitakonurin@gmail.com>
2020-03-02 11:26:34 +0300

    added cards to front-page.php

--- wp-content/themes/aid/front-page.php
+++ wp-content/themes/aid/front-page.php
@@ -27,6 +27,36 @@
                 <hr>
             </div>
         </div>
+        <div class="row d-flex justify-content-between">
+            <?php
+            global $post;
+            $posts = get_posts(array(
+
+                    'numberposts' => 1,
+                    'category' => 2,
+            ));
+            foreach ($posts as $post) {
+                setup_postdata($post);
+                 ?>
+                <div class="col-lg-4 card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <a class="card-img-top" href="<?php the_permalink();?>">
+                                <img class="card-img-top" src="<?php the_post_thumbnail();?>">
+                            </a>
+                            <h5 class="card-title"><?php the_title(); ?></h5>
+                            <p class="card-text"><?php the_excerpt(); ?></p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="<?php the_permalink(); ?>" class="btn btn-red">Перейти к мероприятию</a>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            wp_reset_postdata();
+            ?>
+        </div>
     </div>
 </section>
 <?php
