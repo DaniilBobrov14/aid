825711c8359cfe29ee0f2a5cc5dc50a2310e1e10
Nikita <nikitakonurin@gmail.com>
2020-03-03 11:11:46 +0300

    added autoplay to slider.js

--- wp-content/themes/aid/assets/js/slider.js
+++ wp-content/themes/aid/assets/js/slider.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function() {
     jQuery('.slider-slick-wrapper').slick({
         arrows : false,
         dots : true,
+        autoplay : true,
+        autoplaySpeed: 5000
     });
 
 });
