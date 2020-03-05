ee378567df31173b20f8bd945585b438d9a30400
Nikita <nikitakonurin@gmail.com>
2020-03-05 11:43:03 +0300

    updated tpl_programm.php
    - fixed name of the template
    - added get_header()
    - added get_footer()
    - added global $post and $titlePage, $contentPage variables
    - added html code and echo for php variables

--- wp-content/themes/aid/template-parts/tpl_programm.php
+++ wp-content/themes/aid/template-parts/tpl_programm.php
@@ -1,10 +1,34 @@
 <?php
 /**
-Template Name: Программы
+Template Name: Программа
 Template Post Type: page
  */
+get_header();
 ?>
 
-<div>
-    hello
-</div>
+<?php
+global $post;
+$post = get_post();
+$titlePage = $post->post_title;
+$contentPage = $post->post_content;
+?>
+
+<section class="program">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-title">
+                    <?php echo $titlePage; ?>
+                </h1>
+                <hr class="stroke-hr">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <?php echo $contentPage; ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
