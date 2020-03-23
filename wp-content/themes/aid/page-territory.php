16116e85e43c8ac44198b271717c8a3016336852
Nikita <nikitakonurin@gmail.com>
2020-03-23 15:41:03 +0300

    added global $post and $contentPost

--- wp-content/themes/aid/page-territory.php
+++ wp-content/themes/aid/page-territory.php
@@ -1,3 +1,18 @@
 <?php get_header(); ?>
+<?php
+global $post;
+$post = get_post();
+$titlePost = $post->post_title;
+$contentPost = $post->post_content;
+$authorIDPost = $post->post_author;
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <?php echo $contentPost; ?>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
