b5f49ffbadcf23e9a010f4e1318927d0fbf3e297
Nikita <nikitakonurin@gmail.com>
2020-03-25 10:45:18 +0300

    created tpl_login.php and first html-template for the file

---
+++ wp-content/themes/aid/template-parts/tpl_login.php
@@ -0,0 +1,21 @@
+<?php
+/**Template Name: Авторизация
+Template Post Type: page
+ */
+
+get_header();
+?>
+
+<section class="login-user">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
+
+
