d8fe034082b70d82b446baed382c647da0f134ce
Nikita <nikitakonurin@gmail.com>
2020-03-31 12:37:09 +0300

    added login_in template part

---
+++ wp-content/themes/aid/template-parts/login_in.php
@@ -0,0 +1,6 @@
+<div class="login-in">
+    <a href="<?php echo get_page_uri(140)?>" class="login-in-link registration-link btn rounded">
+        Войти
+        <span class="icon-login"></span>
+    </a>
+</div>
