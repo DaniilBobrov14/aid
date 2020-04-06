5dbe644dbb2872601687ac06d8898135d5f40ac0
Nikita <nikitakonurin@gmail.com>
2020-04-06 16:33:08 +0300

    added script for showing forms

--- wp-content/themes/aid/assets/js/registrationForm.js
+++ wp-content/themes/aid/assets/js/registrationForm.js
@@ -1,5 +1,16 @@
 jQuery(function ($) {
 
+    $('.registration-link').click(function () {
 
+       $('#loginform').attr('hidden' , '');
+       $('#registerform').removeAttr('hidden');
+
+    });
+    $('.login-link').click(function () {
+
+        $('#loginform').removeAttr('hidden');
+        $('#registerform').attr('hidden', '');
+
+    });
 
 });
