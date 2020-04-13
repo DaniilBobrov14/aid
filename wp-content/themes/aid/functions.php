8a530c7859ce535b54d44907254356b67e2f08c5
Bobrov <psdzofficial@yandex.ru>
2020-04-13 15:20:27 +0300

    removed registrationForm.js

--- wp-content/themes/aid/functions.php
+++ wp-content/themes/aid/functions.php
@@ -140,10 +140,6 @@ function aid_scripts() {
 
 	wp_enqueue_script('aid-slider-js' , get_template_directory_uri() . '/assets/js/slider.js');
 
-    wp_enqueue_script('aid-registrationForm-js', get_template_directory_uri() . '/assets/js/registrationForm.js');
-
-//	wp_enqueue_script('aid-tableTimer-js' , get_template_directory_uri() . '/assets/js/tableTimer.js');
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
