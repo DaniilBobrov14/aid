50d5295eea67230e4e8ebba43a5ac7eefec55027
Bobrov <psdzofficial@yandex.ru>
2020-04-14 10:03:54 +0300

    added cancel action for browser when value empty

--- wp-content/themes/aid/assets/js/formValidate.js
+++ wp-content/themes/aid/assets/js/formValidate.js
@@ -5,28 +5,31 @@ jQuery(function ($) {
     {
         $('#registerform').submit(function () {
 
-            event.preventDefault();
-
             if ($('#user_login').val() == '')
             {
+                event.preventDefault();
                 $('.user_login_empty').removeAttr('hidden');
             }
             else if ($('#user_fullname').val() == '')
             {
+                event.preventDefault();
                 $('.user_fullname_empty').removeAttr('hidden');
             }
 
             else if ($('#user_email').val() == '')
             {
+                event.preventDefault();
                 $('.user_email_empty').removeAttr('hidden');
             }
 
             else if ($('#user_password').val() == '')
             {
+                event.preventDefault();
                 $('.user_password_empty').removeAttr('hidden');
             }
             else if ($('#user_passwordVerify').val() == '')
             {
+                event.preventDefault();
                 $('.user_passwordVerify_empty').removeAttr('hidden');
             }
 
