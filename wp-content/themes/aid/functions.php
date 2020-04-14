d30b719eaed4c920f3fb46e0f441ff316c813587
Bobrov <psdzofficial@yandex.ru>
2020-04-14 10:08:28 +0300

    commented aid-formValidate-js

--- wp-content/themes/aid/functions.php
+++ wp-content/themes/aid/functions.php
@@ -140,7 +140,7 @@ function aid_scripts() {
 
 	wp_enqueue_script('aid-slider-js' , get_template_directory_uri() . '/assets/js/slider.js');
 
-	wp_enqueue_script('aid-formValidate-js' , get_template_directory_uri() . '/assets/js/formValidate.js');
+//	wp_enqueue_script('aid-formValidate-js' , get_template_directory_uri() . '/assets/js/formValidate.js');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
