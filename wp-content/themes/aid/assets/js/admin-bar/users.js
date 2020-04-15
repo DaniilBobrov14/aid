2c190d50565c361b1a5282182da1862d9b9a3950
Bobrov <psdzofficial@yandex.ru>
2020-04-15 16:37:25 +0300

    added class for user login for test cycle

--- wp-content/themes/aid/assets/js/admin-bar/users.js
+++ wp-content/themes/aid/assets/js/admin-bar/users.js
@@ -1,8 +1,12 @@
 jQuery(function ($) {
 
+    //добавить классы к оставшимся и удалить комментарий
+
+    let idUser = 2;
+
     $('.add-button').click(function () {
 
-        $('.tbody-data-user').append('<tr class="row-data-user"><td><input type="text" name="user_login" id="user_login" class="input" size="20" value=""></td><td><input type="text" name="user_fullname" id="usesr_fullname" class="input" size="20" value=""></td><td><input type="email" name="user_email" id="user_email" size="20" value=""></td><td><input type="password" name="user_passwordVerify" id="user_passwordVerify" size="20" value=""></td></tr>');
+        $('.tbody-data-user').append('<tr class="row-data-user"><td><input type="text" name="user_login" id="user_login" class="input user_login_input" size="20" value=""></td><td><input type="text" name="user_fullname" id="usesr_fullname" class="input" size="20" value=""></td><td><input type="email" name="user_email" id="user_email" size="20" value=""></td><td><input type="password" name="user_passwordVerify" id="user_passwordVerify" size="20" value=""></td><td hidden><input id="user_id" name="user_id" value="' + idUser++ +'" hidden></td></tr>');
 
     });
 
