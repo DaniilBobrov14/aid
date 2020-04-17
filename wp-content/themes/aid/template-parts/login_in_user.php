8b5f161ac359f1356a2c13e0234a32bd6cea487c
Bobrov <psdzofficial@yandex.ru>
2020-04-17 11:12:51 +0300

    added url to avatar of user

--- wp-content/themes/aid/template-parts/login_in_user.php
+++ wp-content/themes/aid/template-parts/login_in_user.php
@@ -1,6 +1,6 @@
 <?php $userData = get_userdata(get_current_user_id())->data; ?>
 <div class="user-profile">
-    <img class="user-pic rounded-circle" src="http://aid/wp-content/uploads/2020/03/40-409597_male-user-filled-icon-man-icon-png.png.jpg">
+    <img class="user-pic rounded-circle" src="<?php echo get_avatar_url(get_userdata(get_current_user_id())); ?>">
     <span class="user-login-name"><?php echo $userData->user_login; ?></span>
     <span class="icon-down-dir"></span>
 </div>
