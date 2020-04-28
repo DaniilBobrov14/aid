b63cb329346c79052caad575ba4da14231a2d302
Bobrov <psdzofficial@yandex.ru>
2020-04-28 12:53:48 +0300

    added excel reader with PhpSpreedsheet and commented to-do list

--- wp-admin/upload-excel.php
+++ wp-admin/upload-excel.php
@@ -8,6 +8,8 @@
 /** Load WordPress Bootstrap */
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' );
 
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+
 $currentYear = date('Y');
 
 $currentMonth = date('m');
@@ -60,6 +62,45 @@ if (isset($_POST['excel_file_upload'])) {
 
     wp_update_attachment_metadata($attach_id , $attach_data); //обновление метаданных в mysql
 
-    die( json_encode( $data ) );
+//    die( json_encode( $data ) );
+
+    /** Прочтение данных из Excel файла */
+
+    $inputFileType = 'Xlsx';
+    $inputFileName = $filePath;
+    $sheetname = 'Data Sheet #1';
+
+    /** Фильтр для прочтения строк в Excel */
+
+    class ReadFiler implements \PhpOffice\PhpSpreadsheet\Reader\IReadFilter {
+
+        public function readCell($column, $row, $worksheetName = '')
+        {
+            // TODO: Implement readCell() method.
+
+            if ($row >= 2) {
+                return true ;
+            }
+
+            return false ;
+        }
+
+    }
+
+    $filterSubset = new ReadFiler();
+
+    $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
+
+    $reader->setReadFilter($filterSubset);
+
+    $spreadSheet = $reader->load($inputFileName);
+
+    $dataExcel = $spreadSheet->getActiveSheet()->toArray(); //данные из excel файла разбитые в массиве
+
+    ///*добавить импорт определенных данных из экспеля. Например только логин
+    /// возможно нужно делать по колонкам и в массив
+    /// *посмотреть создание пользователей в файле functions.php (функция register_user_callback())
+    /// данные из excel перенести в регистрацию и сделать соответствующее отображение в файле users.php
+
 
 }
