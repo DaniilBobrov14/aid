ab85fd9faab50a9d927ab00743727146a628e993
Bobrov <psdzofficial@yandex.ru>
2020-05-14 12:06:37 +0300

    created getCurrentDate function

--- wp-content/themes/aid/assets/js/getCurrentEventFromProgram.js
+++ wp-content/themes/aid/assets/js/getCurrentEventFromProgram.js
@@ -21,6 +21,41 @@ jQuery(function ($) {
        }
    }
 
+   function getCurrentDate () {
+
+       var dateTimeNow = new Date();
+
+       var dayNow = dateTimeNow.getUTCDate().toLocaleString('ru');
+
+       var monthNow = dateTimeNow.getUTCMonth().toLocaleString('ru');
+
+       var monthsKeys = {
+
+           1 : 'января',
+           2 : 'февраля',
+           3 : 'марта',
+           4 : 'апреля',
+           5 : 'мая',
+           6 : 'июня',
+           7 : 'июля',
+           8 : 'августа',
+           9 : 'сентября',
+           10 : 'октября',
+           11 : 'ноября',
+           12 : 'декабря'
+
+       };
+
+       if (addDataAttributes() == true) {
+
+
+
+       }
+
+   }
+
    addDataAttributes();
+
+   getCurrentDate();
 
 });
