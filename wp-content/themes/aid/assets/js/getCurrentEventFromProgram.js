5e913f0aa855b592a2d1ae8e266f92233d6fc9aa
Bobrov <psdzofficial@yandex.ru>
2020-05-14 11:57:28 +0300

    created function addDataAttributes. Created data-attributes

--- wp-content/themes/aid/assets/js/getCurrentEventFromProgram.js
+++ wp-content/themes/aid/assets/js/getCurrentEventFromProgram.js
@@ -1,5 +1,26 @@
 jQuery(function ($) {
 
-    // $('.table-program').css('background-color' , 'red');
+   function addDataAttributes () {
+
+       if ($('table').hasClass('table-program')) {
+
+           $('.table-row').each(function () {
+
+               let dataDate = $(this).find('.date-cell').text();
+
+               let timeDate = $(this).find('.time-cell').text();
+
+               $(this).find('.date-cell').attr('data-date' , dataDate);
+
+               $(this).find('.time-cell').attr('data-time' , timeDate);
+
+           });
+
+           return true ;
+
+       }
+   }
+
+   addDataAttributes();
 
 });
